66167c6f9f81ed6c64e04c56faeb067c73dd57fd
pulkit-singhall <singhal.pulkit667@gmail.com>
2023-08-08 18:21:35 +0530

    Next Commit

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -9,79 +9,104 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text(
-          "Home Screen",
+          "Currency Converter",
           style: TextStyle(fontSize: 25),
         ),
         elevation: 2.0,
       ),
-      body: const Column(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
+          const Text(
             "Your Answer",
             style: TextStyle(
-              fontSize: 35,
+              fontSize: 30,
             ),
           ),
-          SizedBox(
+          const SizedBox(
             height: 50,
           ),
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Enter Amount in USD",
-              hintStyle: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.grey),
-              prefixIcon: Icon(Icons.attach_money),
-              prefixIconColor: Colors.blue,
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  width: 2.0,
+          Center(
+            child: Container(
+              width: 350,
+              child: const TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Amount in USD",
+                  hintStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey),
+                  prefixIcon: Icon(Icons.attach_money),
+                  prefixIconColor: Colors.blue,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-
-                ),
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              ),
-            ),
-            keyboardType: TextInputType.text,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Enter Amount in INR",
-              hintStyle: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.grey),
-              prefixIcon: Icon(Icons.money_rounded),
-              prefixIconColor: Colors.blue,
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  width: 2.0,
-                ),
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-
-                ),
-                borderRadius: BorderRadius.all(Radius.circular(20)),
+                keyboardType: TextInputType.text,
+                maxLines: 1,
+                minLines: 1,
+                maxLength: 10,
               ),
             ),
-            keyboardType: TextInputType.text,
           ),
-          SizedBox(
-            height: 40,
+          const SizedBox(
+            height: 15,
+          ),
+          Center(
+            child: Container(
+              width: 350,
+              child: const TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Amount in INR",
+                  hintStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey),
+                  prefixIcon: Icon(Icons.money_rounded),
+                  prefixIconColor: Colors.blue,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                ),
+                maxLines: 1,
+                minLines: 1,
+                maxLength: 10,
+                keyboardType: TextInputType.text,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
           ),
           // button
+          ElevatedButton(
+              onPressed: () {
 
+              },
+              child: const Text(
+                "Convert",
+                style: TextStyle(fontSize: 25),
+              ),
+            style: const ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(Colors.black),
+              elevation: MaterialStatePropertyAll(5),
+              fixedSize: MaterialStatePropertyAll(Size(140,45)),
+            ) ,
+          ),
         ],
       ),
     );
